3c1ab5b6aeae9a2eedaec344d5255036aa6b3c1d
Reid Atcheson <reid.atcheson@gmail.com>
2022-03-15 14:40:22 -0500

    fixed build

--- build.rs
+++ build.rs
@@ -1,23 +1,13 @@
 use std::process::Command;
-use std::fs;
-use std::path::PathBuf;
+use std::path::Path;
 use std::env;
 
 fn main() {
-    let args : Vec<String> = env::args().collect();
-    let relfile = PathBuf::from(args[0].clone());
-    let absfile = fs::canonicalize(&relfile);
-    if let Ok(afile) = absfile{
-        if let Some(astr) = afile.to_str(){
-            let abspath = (&astr[0..astr.len()-5]).to_string();
-            let build = abspath.clone() + &"/external/dl_install_metis.sh".to_string();
-            let lib = abspath.clone() + &"/external/lib".to_string();
-
-            let output = Command::new("sh").arg(build).output().unwrap();
-            //println!("{:?}",output);
-            println!("cargo:rustc-link-search={}",lib);
-            println!("cargo:rustc-link-lib=blas");
-            println!("cargo:rustc-link-lib=lapack");
-        }
-    }
+    let dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let build = Path::new(&dir).join("external").join("dl_install_metis.sh");
+    let lib = Path::new(&dir).join("external").join("lib");
+    let _output = Command::new("sh").arg(build.to_str().unwrap()).output().unwrap();
+    println!("cargo:rustc-link-search={}",lib.display());
+    println!("cargo:rustc-link-lib=blas");
+    println!("cargo:rustc-link-lib=lapack");
 }
