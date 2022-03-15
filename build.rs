31a913882b4831a6afdff1c7bb3f40ed311a88ab
Reid Atcheson <reid.atcheson@gmail.com>
2022-03-15 12:55:04 -0500

    switched to use system blas rather than rebuilding openblas every time

--- build.rs
+++ build.rs
@@ -4,5 +4,7 @@ use std::process::Command;
 fn main() {
     let _output = Command::new("sh")
         .arg("./external/dl_install_metis.sh").output();
-    println!("cargo:rustc-link-search=./external/lib")
+    println!("cargo:rustc-link-search=./external/lib");
+    println!("cargo:rustc-link-lib=blas");
+    println!("cargo:rustc-link-lib=lapack");
 }
