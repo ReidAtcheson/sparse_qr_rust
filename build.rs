915fae7a557236e1b20c18bf391901e838eb1062
Reid Atcheson <reid.atcheson@gmail.com>
2022-03-15 15:47:02 -0500

    reverting to openblas system blas would not link

--- build.rs
+++ build.rs
@@ -1,15 +1,8 @@
 use std::process::Command;
-use std::path::Path;
-use std::env;
+
 
 fn main() {
-    let dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let blas = env::var("BLAS").unwrap();
-    let build = Path::new(&dir).join("external").join("dl_install_metis.sh");
-    let lib = Path::new(&dir).join("external").join("lib");
-    println!("cargo:rustc-link-search={}",lib.display());
-    println!("cargo:rustc-link-search={}",blas);
-    println!("cargo:rustc-link-lib=lapack");
-    println!("cargo:rustc-link-lib=blas");
-    let _output = Command::new("sh").arg(build.to_str().unwrap()).output().unwrap();
- }
+    let _output = Command::new("sh")
+        .arg("./external/dl_install_metis.sh").output();
+    println!("cargo:rustc-link-search=./external/lib")
+}
